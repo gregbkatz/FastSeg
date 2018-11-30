4c796a2bb74e2eb7647e230639db7c80295d6b93
greg <gregbkatz@gmail.com>
2018-11-30 00:44:37 +0000

    Now uses classes.txt to map ids. And removed old code

--- CocoDataset.py
+++ CocoDataset.py
@@ -11,7 +11,8 @@ from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torchvision import transforms
+import utils 
 import pdb
 
 
@@ -27,6 +28,8 @@ class CocoDataset(Dataset):
         annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
 
         self.image_path = dataDir + '/images/' + dataType + '/'
+
+        self.classes = utils.Classes(dataDir + 'classes.txt')
 
         self.transform = transform
 
@@ -59,15 +62,12 @@ class CocoDataset(Dataset):
         anns = self.coco.loadAnns(annIds)
 
         class_mask = np.zeros(I.shape[0:2])
-        # instance_mask = np.zeros(I.shape[0:2])
         for ann in anns:
             binary_mask = self.coco.annToMask(ann)
-            class_mask[binary_mask==1] = ann['category_id']
-            # instance_mask[binary_mask==1] = ann['id']
+            class_id = self.classes.getCocoId(ann['category_id'])['id']
+            class_mask[binary_mask==1] = class_id
 
-        curr = set([self.cats[ann['category_id']]['name'] for ann in anns])
-        isPerson = "person" in curr
-        sample = {'image': I, "isPerson": isPerson, 'classMask': class_mask}
+        sample = {'image': I, 'classMask': class_mask}
 
         if self.transform:
             sample = self.transform(sample)
@@ -77,12 +77,12 @@ class CocoDataset(Dataset):
 class Normalize(object):
 
     def __call__(self, sample):
-        image, isPerson, classMask = sample['image'], sample['isPerson'], sample['classMask']
+        image, classMask = sample['image'], sample['classMask']
 
         tf = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         image = tf(image)
 
-        return {'image': image, 'isPerson': isPerson, 'classMask': classMask}
+        return {'image': image, 'classMask': classMask}
 
 class Rescale(object):
     """Rescale the image in a sample to a given size.
@@ -97,22 +97,19 @@ class Rescale(object):
         self.output_size = output_size
 
     def __call__(self, sample):
-        image, isPerson, classMask = sample['image'], sample['isPerson'], sample['classMask']
+        image, classMask = sample['image'], sample['classMask']
 
         image = transform.resize(image, self.output_size, mode='constant', anti_aliasing=True)
         classMask = transform.resize(classMask, self.output_size, mode='constant', anti_aliasing=False, order= 0)
-       
-        #image = transform.resize(image, self.output_size, mode='constant')
-        #classMask = transform.resize(classMask, self.output_size, mode='constant')
 
-        return {'image': image, 'isPerson': isPerson, 'classMask': classMask}
+        return {'image': image, 'classMask': classMask}
 
 
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        image, isPerson, classMask = sample['image'], sample['isPerson'], sample['classMask']
+        image, classMask = sample['image'], sample['classMask']
 
         # swap color axis because
         # numpy image: H x W x C
@@ -120,5 +117,4 @@ class ToTensor(object):
         image = image.transpose((2, 0, 1))
 
         return {'image': torch.from_numpy(image).float(),
-                'isPerson': torch.from_numpy(np.array(float(isPerson))).unsqueeze(0).float(),
                 'classMask': torch.from_numpy(classMask).int()}
