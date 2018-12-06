f649c409239fe970bd793f1b296b1786470d12c1
greg <gregbkatz@gmail.com>
2018-12-06 01:44:05 +0000

    Code clean up and comments

--- UNetSep3.py
+++ UNetSep3.py
@@ -27,7 +27,12 @@ def init_weights(m):
 
 class UNetSep3(nn.Module):
 
-    def sep_conv(self, in_f, out_f, layer):
+    # Returns both regular and separable version of convolution
+    # Creates Sequential modules that include relu    
+    # Includes kaiming initialization
+    # reg = relu(Conv2d(x))
+    # sep = relu(Conv2d(relue(Conv2d(x))))
+    def sep_conv(self, in_f, out_f):
         reg = nn.Conv2d(in_f, out_f, kernel_size=3, stride=1, padding=1, bias=self.bias)
         width = nn.Conv2d(in_f, 1, kernel_size=3, stride=1, padding=1, bias=self.bias)
         depth = nn.Conv2d(1, out_f, kernel_size=1, stride=1, padding=0, bias=self.bias)
@@ -38,42 +43,46 @@ class UNetSep3(nn.Module):
  
         reg = nn.Sequential(reg, self.module_list.relu) 
         sep = nn.Sequential(width, self.module_list.relu, depth, self.module_list.relu)
-        self.module_list.add_module('conv' + layer, reg)
-        self.module_list.add_module('conv' + layer + '_sep', sep)
 
         return reg, sep
 
-    def deconv(self, in_f, out_f, layer):
-        deconv = nn.ConvTranspose2d(in_f, out_f, 2, stride=2, padding=0,
-                                    bias=self.bias)
+    # Returns a deconv module with initialization
+    def deconv(self, in_f, out_f):
+        deconv = nn.ConvTranspose2d(in_f, out_f, 2, stride=2, padding=0, bias=self.bias)
 
         nn.init.kaiming_normal_(deconv.weight, nonlinearity='relu')
 
-        self.deconv_layers.append(deconv)
         return deconv
 
-    def conv_layer(self, i, in_channel, start_filters):
+    # Creates encode, deconv, and decode modules for a layer
+    def add_layer(self, i, in_channel, start_filters):
+        # a and b are for encoding
         if i == 0:
-            reg_a, sep_a = self.sep_conv(in_channel, start_filters, '1a')
+            reg_a, sep_a = self.sep_conv(in_channel, start_filters)
         else: 
-            reg_a, sep_a = self.sep_conv(2**(i-1)*start_filters, 2**i*start_filters, str(i+1) + 'a') 
+            reg_a, sep_a = self.sep_conv(2**(i-1)*start_filters, 2**i*start_filters) 
 
-        reg_b, sep_b = self.sep_conv(2**i*start_filters, 2**i*start_filters, str(i+1) + 'b') 
-        reg_c, sep_c = self.sep_conv(2**(i+1)*start_filters, 2**i*start_filters, str(i+1) + 'c') 
-        reg_d, sep_d = self.sep_conv(2**i*start_filters, 2**i*start_filters, str(i+1) + 'd') 
+        reg_b, sep_b = self.sep_conv(2**i*start_filters, 2**i*start_filters) 
 
+        # Including max pooling at beginning of encoding layers with exception
+        # of the first layer
         if i == 0:
             encode = nn.Sequential(sep_a, sep_b)
         else: 
             encode = nn.Sequential(self.module_list.pool, sep_a, sep_b) 
 
+        # c and de are for decoding
+        reg_c, sep_c = self.sep_conv(2**(i+1)*start_filters, 2**i*start_filters) 
+        reg_d, sep_d = self.sep_conv(2**i*start_filters, 2**i*start_filters) 
         decode = nn.Sequential(reg_c, reg_d)
  
+        # There are one fewer deconv layers so skip it on the first layer
+        deconv = None
         if i > 0:
-            deconv = self.deconv(2**(i)*start_filters, 2**(i-1)*start_filters, str(i+1))
+            deconv = self.deconv(2**(i)*start_filters, 2**(i-1)*start_filters)
 
-        self.encode_layers.append(encode)
-        self.decode_layers.append(decode)
+        return encode, deconv, decode
+
 
 
     def __init__(self, in_channel, num_classes=3, start_filters=64, num_batchnorm_layers=0, dropout=0.1):
@@ -91,17 +100,22 @@ class UNetSep3(nn.Module):
 
         nlayers = 5
         for i in range(nlayers):
-            self.conv_layer(i, in_channel, start_filters)
-        
+            encode, deconv, decode, = self.add_layer(i, in_channel, start_filters)
+            self.encode_layers.append(encode)
+            self.decode_layers.append(decode)
+            if deconv: 
+                self.deconv_layers.append(deconv)
+
+
+        # Add one more convlution for final output        
         conv_out = nn.Conv2d(start_filters, num_classes, kernel_size=1, stride=1, padding=0)
         nn.init.kaiming_normal_(conv_out.weight, nonlinearity='relu')
         self.module_list.add_module('conv_out', conv_out)
 
-        #self.apply(init_weights)
-
     def forward(self, x):
 
         # Encode 
+        # Cache activations to pass into decoder
         a = []
         a.append(x)
         for i in range(len(self.encode_layers)):
